988dbe2706c7d81b2f69d1b012bbdaaf6df3c090
XenuGodOfChild <aidanalvillar@gmail.com>
2023-10-09 00:28:17 -0400

    update script

--- vr-project1/Assets/AidanStuff/CornholeScript.cs
+++ vr-project1/Assets/AidanStuff/CornholeScript.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class CornholeScript : MonoBehaviour
 {
+    [SerializeField] GameObject bag;
+    [SerializeField] Transform bagSpawn;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnBag();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void spawnBag()
+    {
+        GameObject.Instantiate<GameObject>(bag, bagSpawn);
+    }
 }
