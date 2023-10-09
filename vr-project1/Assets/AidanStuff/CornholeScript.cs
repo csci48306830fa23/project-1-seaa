481140113666aa987570967d076f11b555a2f2f4
XenuGodOfChild <aidanalvillar@gmail.com>
2023-10-09 00:27:28 -0400

    Update CornholeScript.cs

--- vr-project1/Assets/AidanStuff/CornholeScript.cs
+++ vr-project1/Assets/AidanStuff/CornholeScript.cs
@@ -2,21 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:vr-project1/Assets/AidanStuff/CornholeScript.cs
 public class CornholeScript : MonoBehaviour
-========
-public class Trophy1 : MonoBehaviour
->>>>>>>> 270e33e39fe8e4481b03068196b3fdd135439491:vr-project1/Assets/EliePrefabs/Token.cs
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
