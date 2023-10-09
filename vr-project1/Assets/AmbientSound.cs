648583037c84c3264db04df809cf99feb21ac138
sorae <46469913+soraeee@users.noreply.github.com>
2023-10-09 17:44:28 -0400

    audio?

---
+++ vr-project1/Assets/AmbientSound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AmbientSound : MonoBehaviour
+{
+
+    [SerializeField]
+    AudioClip ambient;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ambient.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
