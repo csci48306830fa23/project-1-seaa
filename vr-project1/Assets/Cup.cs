418e386ae8794e03e299073bf05a022e5b7fc316
sorae <46469913+soraeee@users.noreply.github.com>
2023-10-08 23:03:05 -0400

    oh no a tween

--- vr-project1/Assets/Cup.cs
+++ vr-project1/Assets/Cup.cs
@@ -20,7 +20,7 @@ public class Cup : MonoBehaviour
                 main.win();
             } else
             {
-                main.loss();
+                main.loss(cupNum);
             }
         };
     }
