11d0709a41fc792b169a01bac7cbfece3522616a
egelman <118682333+egelman@users.noreply.github.com>
2023-10-10 11:28:23 -0400

    change deposit logic

--- vr-project1/Assets/EliePrefabs/TrophyBox.cs
+++ vr-project1/Assets/EliePrefabs/TrophyBox.cs
@@ -7,12 +7,9 @@ public class TrophyBox : MonoBehaviour
 {
     public int tokenCount = 0;
     public GameData gameData;
-    [SerializeField]
-    GameObject player;
     public GameObject tokenPrefab;
     public GameObject trophyPrize; 
     public Transform trophySpawnPoint; 
-    public int coinsToSpawn = 3;
     public Vector3 spawnOffset = new Vector3(0, 1, 0);
     public Button depositButton;
 
@@ -25,23 +22,17 @@ public class TrophyBox : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-         if (other.gameObject == player)
+         if (collision.gameObject.CompareTag("Token"))
         {
-            if (CheckTokens())
+            Destroy(collision.gameObject);
+            tokenCount++;
+
+            if (tokenCount >= 2)
             {
-                depositButton.gameObject.SetActive(true);
+                GiveTrophy();
             }
-                
         }
 
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == player)
-        {
-            depositButton.gameObject.SetActive(false);
-        }
     }
 
     private bool CheckTokens()
@@ -49,35 +40,7 @@ public class TrophyBox : MonoBehaviour
         return (gameData.tokensCollected >= 2);
        
     }
-    public void OnDepositButtonClicked()
-    {
-        Renderer trophyBoxRenderer = GetComponent<Renderer>();
-        Vector3 topOfBox = trophyBoxRenderer.bounds.max;  
-        Vector3 spawnPosition = topOfBox + new Vector3(0, 0.5f, 0);         
-        
-        for (int i = 0; i < coinsToSpawn; i++)
-        {
-            Vector3 offsetPosition = spawnPosition + new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(0, 0.2f), Random.Range(-0.1f, 0.1f));  // Randomizes the position a bit for each coin
-             Instantiate(tokenPrefab, offsetPosition, Quaternion.identity);        
-        }
-        
-        depositButton.gameObject.SetActive(false);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Token"))
-        {
-            Destroy(collision.gameObject);
-            tokenCount++;
-
-            if (tokenCount >= coinsToSpawn)
-            {
-                GiveTrophy();
-            }
-        }
-    }
-
+  
     void GiveTrophy()
     {
         Instantiate(trophyPrize, trophySpawnPoint.position, Quaternion.identity);
